598bca92f3c5d2fcd6ed45f0b973a93374346ead
Erik Mulder <emulder@bol.com>
2018-10-17 11:19:40 +0200

    Testing training assignments

--- dags/training.py
+++ dags/training.py
@@ -119,7 +119,7 @@ class HttpToGcsOperator(BaseOperator):
 
         gcs = GoogleCloudStorageHook()
         gcs.upload(bucket="airflow-training", object=self.gcs_path,
-                   filename="/tmp/response", mime_type="application/json")
+                   filename=str(text_file), mime_type="application/json")
 
 
 http_to_gcs = HttpToGcsOperator(
